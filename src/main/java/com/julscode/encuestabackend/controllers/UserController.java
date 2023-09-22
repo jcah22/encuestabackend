a83cde0c743f36cf548be27738369cac22892612
julio <jcah549@gmail.com>
2023-09-22 17:24:24 -0600

    segundo commit

---
+++ src/main/java/com/julscode/encuestabackend/controllers/UserController.java
@@ -0,0 +1,22 @@
+package com.julscode.encuestabackend.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping()
+    public String createUser(){
+        return "Create User";
+    }
+
+
+
+
+
+
+
+}
